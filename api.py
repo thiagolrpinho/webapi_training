52a9f40df2ca148840bc645d6afed196fd318a8c
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-09 10:33:44 -0300

    :sparkles: Add: Return books by id

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -22,7 +22,7 @@ books = [
      'first_sentence': 'to wound the autumnal city.',
      'published': '1975'}
 ]
-    
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -30,9 +30,32 @@ def home():
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
-# A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
     return jsonify(books)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
 
 app.run()
